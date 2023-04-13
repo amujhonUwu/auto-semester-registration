225171f7174cd46f51daf13d3f7c3fafb4e3b5fc
amujhon <jhonalexandrocm@gmail.com>
2023-04-13 16:05:26 -0500

    The code is now more accesible, modificable and legible.

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import datetime
 
 URL = "https://preinscripcion.utm.edu.ec/login"
 
+# Ingresa aquí tu correo y tu contraseña
+EMAIL = ""
+PASSWORD = ""
+
+# Definiendo variables generales
+
 ID_USER = "txt_usuario"
 ID_PASSWORD = "txt_password"
 ID_SIDEBAR = "sidebar-menu"
@@ -18,6 +24,10 @@ ID_PERIODO = ""
 
 CLASS_BUTTON = "ant-btn"
 
+TIEMPO_DE_EJECUCION = datetime.time(10, 00, 00)
+TIEMPO_DE_ESPERA = 0.1  # <------- Aquí puedes modificar el tiempo de espera entre los cambios realizados.
+
+# Ingresa aquí tu Malla y tu Carrera correspondientes.
 MALLA = "SISTEMAS DE INFORMACION 2017 (REDISEÑO 2019)"
 CARRERA = "Ingenieria De Sistemas Informaticos"
 
@@ -32,21 +42,31 @@ def no_se_encuentra_opcion(nombre_opcion:str, segundos_de_espera:float = 0):
     print(f"No se encuentra la opción/boton [{nombre_opcion}]. Esperando {segundos_de_espera} segundos... ")
     time.sleep(segundos_de_espera)
 
+def seleccionar_opcion_acordeon(id_opcion:str, opcion_visible:str):
+    """ Selecciona alguna opción por medio de texto visible (Usar en caso de acordeones). 
+
+    * id_opcion: Id del acordeón.
+
+    * opcion_visible: Texto de la opción a seleccionar.
+    """
+
+    opcion = Select(driver.find_element(By.ID, ID_MALLA))
+    opcion.select_by_visible_text("MAYO DEL 2023 HASTA SEPTIEMBRE DEL 2023")
+
+# ----- Script en ejecución -----
+
 driver = webdriver.Chrome()
 driver.get(URL)
-
-ten_am = datetime.time(10, 00, 00)
 
 user = driver.find_element(By.ID, ID_USER)
 password = driver.find_element(By.ID, ID_PASSWORD)
 button = driver.find_element(By.CLASS_NAME, CLASS_BUTTON)
 
-time.sleep(0.1)
-user.send_keys("jcedeno7718@utm.edu.ec")
-time.sleep(0.1)
-password.send_keys("Pochita14")
+user.send_keys(EMAIL)
+password.send_keys(PASSWORD)
+    
 
-while datetime.datetime.now().time() < ten_am:
+while datetime.datetime.now().time() < TIEMPO_DE_EJECUCION:
     pass
 
 # Ingresa en las inscripciones
@@ -54,7 +74,7 @@ button.click()
 
 while(True):
     try:
-        time.sleep(0.1)
+        time.sleep(TIEMPO_DE_ESPERA)
         # Selecciona la opción de inscripción
         inscripcion = driver.find_element(By.ID, ID_INSCRIPCION)
         inscripcion.click()
@@ -63,21 +83,20 @@ while(True):
         no_se_encuentra_opcion("Inscripcion", 5)
         driver.refresh() 
 
+time.sleep(TIEMPO_DE_ESPERA)
 
-time.sleep(0.1)
+
 # Selecciona la carrera 
-carrera = Select(driver.find_element(By.ID, ID_CARRERA))
-carrera.select_by_visible_text(CARRERA)
+seleccionar_opcion_acordeon(ID_CARRERA, CARRERA)
+time.sleep(TIEMPO_DE_ESPERA)
 
-time.sleep(0.1)
 # Selecciona la Malla 
-malla = Select(driver.find_element(By.ID, ID_MALLA))
-malla.select_by_visible_text("SISTEMAS DE INFORMACION 2017 (REDISEÑO 2019)")
+seleccionar_opcion_acordeon(ID_MALLA, MALLA)
+time.sleep(TIEMPO_DE_ESPERA)
 
-time.sleep(0.2)
 # Selecciona el periodo 
-periodo = Select(driver.find_element(By.ID, ID_MALLA))
-periodo.select_by_visible_text("MAYO DEL 2023 HASTA SEPTIEMBRE DEL 2023")
+seleccionar_opcion_acordeon(ID_PERIODO, CARRERA)
+time.sleep(TIEMPO_DE_ESPERA)
 
 while(True):
     pass
